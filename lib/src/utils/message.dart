4b4767ce04ac1b8d071b2aa25b72c9ad00d9c502
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2023-10-23 16:41:36 -0300

    feat: toast messages

---
+++ lib/src/utils/message.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+abstract class MessageApp {
+  static const errorPasswordMismatch = 'Senha e Confirmação não conferem';
+  static const errorWrongLoginPassword = 'Login ou Senha incorretos';
+
+  static toastMesssage(
+    BuildContext context,
+    String message, {
+    Color color = Colors.red,
+    IconData icon = Icons.error,
+    void Function()? f,
+  }) {
+    FToast fToast = FToast();
+    fToast.init(context);
+    fToast.showToast(
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: color,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              onPressed: f,
+              icon: Icon(icon),
+              color: Colors.white,
+            ),
+            Text(
+              message,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+          ],
+        ),
+      ),
+      toastDuration: const Duration(
+        seconds: 3,
+      ),
+    );
+  }
+}
