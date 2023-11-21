3723153db0589e0b54090087e967d5f09f26ae93
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2023-11-21 01:42:43 -0300

    feat: create home page

---
+++ lib/src/views/home.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the LoginPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextButton.icon(
+              onPressed: () {
+                // TODO: Load Pessoas
+              },
+              icon: const Icon(Icons.person, size: 80),
+              label: const Text("Pessoas")),
+          TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  // TODO: Load Imoveis
+                });
+              },
+              icon: const Icon(Icons.house, size: 80),
+              label: const Text("Imóveis")),
+          TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  // TODO: Load Vendas
+                });
+              },
+              icon: const Icon(Icons.monetization_on, size: 80),
+              label: const Text("Vendas")),
+        ],
+      ),
+    );
+  }
+}
